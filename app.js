607626c1310115096aa415f444ed4734183c072b
LivinkumarS <001liv100@ggmail.com>
2025-05-21 15:39:26 +0530

    Variables, Primitive DataTypes (String)

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
+// let word = "Hello world";
+// word = "hello";
+
+// console.log(word);
+
+// String
+
+// let firstName = `Livin`;
+// let LastName = "Kumar";
+
+// console.log(firstName+LastName);
+
+// let Name="I likE coDing";
+// console.log(Name.length);
+// console.log(Name.toUpperCase());
+// console.log(Name.toLowerCase());
+// console.log(Name.charAt(3));
+// console.log(Name.includes(" co"));
+
+// let Name = "Saala";
+// let age = 21;
+// console.log(`Hello ${Name}!. Your Age is ${age}`);
+
+// Number
+
+// let num1=21
+// let num2=-12
+// let num3=3.33
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// Boolean
+
+// let isOk = true;
+// let isAllOk = false;
+
+// console.log(isOk);
+// console.log(isAllOk);
+
+// Undefined
+// let data;
+// console.log(data);
+
+// Null
+
+// let data = null;
+
+// console.log(data);
