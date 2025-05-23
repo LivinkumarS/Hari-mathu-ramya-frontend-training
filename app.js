f14b8405880f9df6a398b422e4b2cb781cea69f3
LivinkumarS <001liv100@ggmail.com>
2025-05-23 15:58:38 +0530

    Comparison Operator

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-// Assignment Operator
+// Comparison operator
 
 let a = 10;
 
-a += 10; //20
-a -= 10; //10
-a *= 10; //100
-a /= 10; //10
-a %= 10; //0
-a **= 10;
+// console.log("Hello" == "hello");
 
-console.log(a);
+// console.log(10 === "10");
 
+//console.log(a != 11); //a is not equal to 10
+
+console.log(a >= 20); 
