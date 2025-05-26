0c8752075b3b15f3804cb2dcdcc7213dcf6ccaf6
LivinkumarS <001liv100@ggmail.com>
2025-05-26 14:36:28 +0530

    Ternary Operator and Conditional

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-// Logical Operator
+// Ternary Operator
 
-// console.log(true && false);
-// console.log(true || true);
+// let num1 = 10;
+// let num2 = 20;
 
-// console.log(!false);
+// num1 < num2
+//   ? console.log("num1 is greater than num2")
+//   : console.log("num2 is greater than num1");
 
-// console.log(!(!true || false) && 1 <= 10); //r-t, m-t, h-t
+// Conditional Clauses
 
-// let a = true;
+let num1 = 11;
+// let num2 = 20;
 
-// console.log(!((a && true) || !a || (a && !(a + 10 < a / 10))) && !a);
+// if (num1 > num2) {
+//   console.log("You are in the first condition");
+// } else {
+//   console.log("You are in the second condition");
+// }
+
+// if (num1 > 8) {
+//   console.log("Greater");
+// } else if (num1 > 10) {
+//   console.log("Average");
+// } else {
+//   console.log("Less");
+// }
+
+let mark= 100;
+
+// Rules:
+// if 90< mark <=100
+// if 80< mark <=90
+// if 70< mark <=80
+
+// if (mark > 90 && mark <= 100) {
+//   console.log("Grade A");
+// } else if (mark > 80 && mark <= 90) {
+//   console.log("Grade B");
+// } else if (mark > 70 && mark <= 80) {
+//   console.log("Grade C");
+// } else {
+//   console.log("Grade D");
+// }
