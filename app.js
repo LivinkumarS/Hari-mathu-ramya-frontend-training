d2a407777f780dd0ce87dc9d9a73de776dd49324
LivinkumarS <001liv100@ggmail.com>
2025-05-26 14:44:30 +0530

    Truthy and falsy values

--- app.js
+++ app.js
@@ -1,44 +1,5 @@
-// Ternary Operator
+let x;
 
-// let num1 = 10;
-// let num2 = 20;
-
-// num1 < num2
-//   ? console.log("num1 is greater than num2")
-//   : console.log("num2 is greater than num1");
-
-// Conditional Clauses
-
-let num1 = 11;
-// let num2 = 20;
-
-// if (num1 > num2) {
-//   console.log("You are in the first condition");
-// } else {
-//   console.log("You are in the second condition");
-// }
-
-// if (num1 > 8) {
-//   console.log("Greater");
-// } else if (num1 > 10) {
-//   console.log("Average");
-// } else {
-//   console.log("Less");
-// }
-
-let mark= 100;
-
-// Rules:
-// if 90< mark <=100
-// if 80< mark <=90
-// if 70< mark <=80
-
-// if (mark > 90 && mark <= 100) {
-//   console.log("Grade A");
-// } else if (mark > 80 && mark <= 90) {
-//   console.log("Grade B");
-// } else if (mark > 70 && mark <= 80) {
-//   console.log("Grade C");
-// } else {
-//   console.log("Grade D");
-// }
+if (x) {
+  console.log("True");
+}
