a0e4b2e82e217101fd84824a9d25ef4e8d66909e
LivinkumarS <001liv100@ggmail.com>
2025-06-03 16:05:34 +0530

    Loops in JS

--- app.js
+++ app.js
@@ -1,5 +1,72 @@
-let x;
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
 
-if (x) {
-  console.log("True");
+//10 times
+
+// i = 0,-1,-2,-100,,
+
+// 1,2
+
+// While Loop
+
+//let i = 0; //0,1,2.....9,10
+
+// while (i < 10) {
+//   console.log("Hello world");
+//   i++;
+// }
+
+// let continueLoop = true;
+// let secret = 55;
+
+// while (continueLoop) {
+//   let number = prompt("Guess the number: ");
+//   if (parseInt(number) === secret) {
+//     continueLoop = false;
+//     console.log("Congrats You Won!!!");
+
+//   }
+// }
+
+// let ans = prompt("Type a number hari: ");
+
+// console.log(ans);
+
+//33
+
+// Do while
+
+// let i = 10;
+
+// console.log("From Do while");
+
+// do {
+//    console.log(i);
+//   i++;
+// }while (i < 10);
+
+// let a = 10;
+
+// console.log("From While");
+
+// while (a < 10) {
+//   console.log(a);
+//   a++;
+// }
+
+// Break and Continue
+
+let i = 1; //10,11
+
+
+
+
+while (i <= 100) {
+  if (i === 10) {
+    i++;
+    continue;
+  }
+  console.log(i);
+  i++;
 }
