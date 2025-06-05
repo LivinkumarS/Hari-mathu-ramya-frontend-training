5008f616f4d0f77994ddcc85817062ee0e7c4f1c
LivinkumarS <001liv100@ggmail.com>
2025-06-05 15:03:05 +0530

    Task Update

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
-let arr = [
-  "apple",
-  "orange",
-  "pineapple",
-  "papaya",
-  "banana",
-  1,
-  2,
-  3,
-  4,
-  true,
-  false,
-];
+function printName(name){
+ 
+}
 
-let arr1 = [1, 132, 234, 32, 234, 3, 324, 54, 56, 5, 5, 54, 34, 324];
+printName("saala");
 
-let arr2 = ["apple", "orange", "pineapple", "papaya", "banana"];
 
-// console.log(arr.length);
-// console.log(arr[6]);
-
-// arr[2]="hddfhghre"
-
-// arr.push("haggududy");
-// arr.pop();
-// arr.pop();
-// arr.unshift(0);
-// arr.unshift(-1);
-// arr.shift();
-// arr.splice(2, 5, 23, 24, 25, "shguer", true);
-// arr.splice(0, 11, 1, 2, 3);
-// arr2.sort((a, b) => {
-//   return a - b;
-// });
-// arr.reverse();
-
-console.log(arr);
+console.log("s");
+console.log("a");
+console.log("a");
+console.log("l");
+console.log("a");
