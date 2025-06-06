c032ae04409d333c3ded6eb75c83696852693b4c
LivinkumarS <001liv100@ggmail.com>
2025-06-06 12:57:13 +0530

    Array Basic Methods

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-function printName(name){
- 
-}
+// let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-printName("saala");
+// let list1 = [432, 31, 3, 3124, 356, 234, 3];
+
+// let newList = list.concat(list1);
+// let newList = list1.slice(3, 9);
+// console.log(newList);
+// let str = list.join("");
+// console.log(str);
+
+// let index = arr.indexOf(101);
+// console.log(index);
+
+// console.log(arr[9]);
+// console.log(arr.indexOf(10));
+
+let arr = [
+  "apple",
+  "banana",
+  "orange",
+  "grape",
+  "kiwi",
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  true,
+  false,
+];
+console.log(arr.includes(arr[5] > arr[6])); //true,false.
 
 
-console.log("s");
-console.log("a");
-console.log("a");
-console.log("l");
-console.log("a");
+// console.log(list);
+// console.log(list1);
