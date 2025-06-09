9a292d705276a0c94192053155ac2e1fe2359068
LivinkumarS <001liv100@ggmail.com>
2025-06-09 12:17:31 +0530

    Array Traversal

--- app.js
+++ app.js
@@ -1,51 +1,23 @@
-// Map
+let arr = [
+  "apple",
+  242353,
+  234,
+  3,
+  356,
+  432,
+  true,
+  false,
+  [0, 1, 2, 3, 4, 5, [-1, -1, -3, ["apple", "orange", "kiwi"]]],
+  () => {
+    console.log("Hello World");
+  },
+];
 
-// [1,2,3,4,5,6,7,8,9,10]--->[3,4,5,6,7,8,9,10,11,12]Transformation-->+2
+// function hello() {
+//   console.log("Hello world");
+// }
 
-// let arr = ["Apple", "Orange", "Pineapple", "Banana", "Kiwi"];
+// hello();
 
-// let newArr = arr.map((item, ind) => {
-//   return `${item} is a fruit`;
-// });
-
-// console.log(newArr);
-
-//["Apple is a fruit", "Orange is a fruit"....]
-
-// ("Apple", 0), ("Orange", 1),.......("Kiwi", 4).
-
-// Filter
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10];
-
-// let newArr = arr.filter((item, ind) => {// 2,1
-//   return ind-1;
-// }); //-1,0,1,2,3,4,5,6
-
-// console.log(newArr);
-
-//(1,0),(2,1)....(0,9),(10,10)
-
-// []
-
-// Reduce
-
-// [1,2,3,4,5,6,7,8,9,10]--> 55
-//["hello ","Guys, ","This is ","LK here"]--->
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //-55
-
-// let newArr = arr.reduce((acc, item, ind) => {
-//   return acc * item;
-// }, 1);
-
-
-
-// console.log(newArr);
-
-//-1,
-
-// "hello "
-
-// (0,1,0),(1,2,1),(3,3,2),(6,4,3),(10,5,4),(15,6,5),(21,7,6),(28,8,7),(36,9,8),(45,10,9)--->55
-
-// iter 1-->
+// console.log(arr[8][6][3][2][3]);
+console.log(arr[0][4]);
