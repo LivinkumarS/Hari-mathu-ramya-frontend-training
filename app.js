8b60706db0e8f2d4e2014bc795329e5611b1f4d2
LivinkumarS <001liv100@ggmail.com>
2025-06-10 12:08:16 +0530

    for of and for in loops

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
 // let obj = {
-//   name: "Livinkumar",
-//   age: 88,
+//   fname: "harikrishnan",
+//   lname: "Ramasamy",
 //   canVote: true,
+//   marks: 90,
+//   favSubjects: [
+//     "html",
+//     "css",
+//     "js",
+//     { mostFav: "react", leastfav: ["angular", "node"] },
+//   ],
+//   print() {
+//     console.log(this.fname);
+//   },
 // };
 
-// let arr = ["Livinkumar", 88, true];
+// for (let key in obj) {
+//   console.log(obj[key]);
+// }
 
-// console.log(obj.canVote);
-// console.log(obj["canVote"]);
+// fname, lname...
 
-// console.log(arr[2]);
+let fname = "Hari";
 
-// this keyword
+let arr = [213, 32, 34, 32, 45, 32, 56, 234, 578];
 
-let obj = {
-  fname: "harikrishnan",
-  lname: "Ramasamy",
-  canVote: true,
-  marks: 90,
-  favSubjects: [
-    "html",
-    "css",
-    "js",
-    { mostFav: "react", leastfav: ["angular", "node"] },
-  ],
-  print() {
-    console.log(this.fname);
-  },
-};
+for (let num of arr) {
+  console.log(num);
+}
 
-// obj["print"]();
-
-// let keys = Object.keys(obj);
-// console.log(keys);
-
-// let val = Object.values(obj);
-// console.log(val);
-
-console.log(obj.favSubjects[3].leastfav[1]);
+for (let char of fname) {
+  console.log(char);
+}
