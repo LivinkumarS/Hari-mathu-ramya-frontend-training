835f3676f5e3b31d0c14f8eac11d94b1bde56000
LivinkumarS <001liv100@ggmail.com>
2025-06-11 12:00:06 +0530

    NaN

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
-// let arr = [231, 34, 32];
+// let num = Number("234");
+// let num = 0 / 0;
+// console.log(num);
 
-// // let n1 = arr[0];
-// // let n2 = arr[1];
-// // let n3 = arr[2];
+let a;
+let b = null;
+let c = 0 / 0;
 
-// let [n1, n2, n3] = arr;
+// console.log(c);
+console.log(isNaN(c));
 
-// // console.log(n1, n2, n3);
+//1,2
 
-// let { fname, lname, age } = obj;
-
-// // console.log(fname, lname, age);
-
-let arr = [1, 2, 3, 4, 5];
-let arr2 = [-1, ...arr, -2, -3];
-
-// arr2 = arr2.concat(arr);
-
-// console.log(arr2);
-let obj = {
-  fname: "Saala",
-  lname: "kumar",
-  age: 80,
-};
-
-let obj1 = {
-  ...obj,
-  role: "dev",
-  salary: "2M",
-};
-
-const { age, lname, ...rest } = obj1;
-console.log(age);
-console.log(lname);
-console.log(rest);
+// console.log(typeof c);
