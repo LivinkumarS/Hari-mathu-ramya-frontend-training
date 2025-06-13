b6513ff995733fe03c3eee438d347160f5417236
LivinkumarS <001liv100@ggmail.com>
2025-06-13 12:08:03 +0530

    Create and Inserte elements

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
-// const h1 = document.getElementById("head");
-// console.log(h1.textContent);
-// h1.textContent = "Title";
-// h1.innerHTML = "<span>Title</span>";
+let newElement = document.createElement("div");
+newElement.textContent = "New Element Created in JavaScript!";
+newElement.style.color = "#657547";
+newElement.style.fontSize = "50px";
+// newElement.textContent = "";
 
-// console.log(h1.getAttribute("id"));
-// console.log(a.getAttribute("href"));
-// console.log(a.textContent);
+// document.querySelector("body").appendChild(newElement);
+// document.querySelector("body").prepend(newElement);
 
-// a.setAttribute("target", "_blank");
-// a.setAttribute("href", "https://www.yahoo.com");
-// a.textContent = "Yahoo!";
-// a.removeAttribute("target");
-// h1.removeAttribute("id");
-
-// h1.style.backgroundColor = "red";
-// h1.style.color = "blue";
-// h1.style.display = "inline";
-
-// console.log(h1.classList);
-// h1.classList.add("title");
-// h1.classList.add("title1");
-// h1.classList.add("title2");
-
-// h1.classList.remove("head1");
-
-// h1.classList.toggle("head");
-// h1.classList.toggle("head4");
-
-
-// let ul = document.querySelector("ul");
-// console.log(ul.children);
-// console.log(ul.firstElementChild);
-// console.log(ul.lastElementChild);
-
-// console.log(ul.firstElementChild.parentElement);
-
-// console.log(ul.firstElementChild.nextElementSibling);
-// console.log(ul.lastElementChild.previousElementSibling);
-
-const a = document.querySelector("a");
-console.log(a.parentElement.firstElementChild.lastElementChild);
+document
+  .querySelector("body")
+  .insertBefore(newElement, document.querySelector("h2"));
